79f156c33e63ac4c6d605e67e8fe59d39fff07df
Daniil <laito.treasure@gmail.com>
2021-08-03 18:37:18 +0300

    organic-1_page-header: Modified and update all of fiels to the last version

--- src/js/app.js
+++ src/js/app.js
@@ -8,5 +8,24 @@ import dots from './modules/dots';
 
 // When DOM is ready
 $(() => {
-  dots();
+    const $body = $('body');
+    const $btnHamburger = $('.btn-hamburger');
+    const $nav = $('.nav');
+    const MODAL_OPENED_CLASS_NAME = 'is-modal-opened';
+    const BTN_HAMBURGER_ACTIVE_CLASS_NAME = 'is-opened';
+    const NAVIGATION_ACTIVE_CLASS_NAME = 'is-opened';
+  
+    $btnHamburger.on('click', () => {
+      $body.toggleClass(MODAL_OPENED_CLASS_NAME);
+      $btnHamburger.toggleClass(BTN_HAMBURGER_ACTIVE_CLASS_NAME);
+      $nav.toggleClass(NAVIGATION_ACTIVE_CLASS_NAME);
+    });
+
+    $(window).scroll(function(){
+      var header = $('.header'),
+          scroll = $(window).scrollTop();
+    
+      if (scroll >= 100) header.addClass('fixed');
+      else header.removeClass('fixed');
+    });
 });
